dd2f8442a2883560acac96dc8dfcc0cae1f7a01d
slbejaranom <slbejaranom@gmail.com>
2021-03-27 16:11:11 -0500

    Toda la API funcionando

--- TicketApi/Controllers/TicketsController.cs
+++ TicketApi/Controllers/TicketsController.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 
 namespace TicketApi.Controllers{
 
-    [Route ("api/[controller]")]
+    [Route ("api/ticket")]
     [ApiController]
     public class TicketsController : ControllerBase{
         private readonly TicketService _ticketService;
@@ -16,8 +16,50 @@ namespace TicketApi.Controllers{
 
         [HttpGet]
         public ActionResult<List<Ticket>> Get() =>
-            _ticketService.Get();
+            _ticketService.Get();       
 
-        
+        [HttpGet("{id}", Name = "GetTicket")] 
+        public ActionResult<Ticket> Get(string id){
+            var ticket = _ticketService.Get(id);
+
+            if(ticket == null){
+                return NotFound();
+            }
+            
+            return ticket;
+        }
+
+        [HttpPost]
+        public ActionResult<Ticket> Create(Ticket ticket){
+            _ticketService.Create(ticket);
+
+            return CreatedAtRoute("GetTicket", new { id = ticket.id.ToString() }, ticket);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(string id, Ticket ticketIn){
+            var ticket = _ticketService.Get(id);
+
+            if(ticket == null){
+                return NotFound();
+            }
+
+            _ticketService.Update(id, ticketIn);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(string id){
+            var ticket = _ticketService.Get(id);
+
+            if(ticket == null){
+                return NotFound();
+            }
+
+            _ticketService.Remove(ticket.id);
+
+            return NoContent();
+        }
     }
 }
